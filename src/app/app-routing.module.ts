cba4c5094977359d563218dfa5af0e7ecb24df11
Krzysztofer666 <krzych.janus@gmail.com>
2022-03-07 13:51:12 +0100

    Poprawiono route "o nas"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageModule } from './pages/home.page-module';
+import { AboutUsPageModule } from './pages/about-us.page-module';
 
 const routes: Routes = [{ 
         path: '', 
         loadChildren: () => HomePageModule
-      }];
+      },
+  { 
+        path: 'o-nas', 
+        loadChildren: () => AboutUsPageModule
+      }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
