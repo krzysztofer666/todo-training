570e88b5650a20b2d5e6274e4052068af564f0e9
Krzysztofer666 <krzych.janus@gmail.com>
2022-03-16 11:16:57 +0100

    L2E5 - Stworzono smart component employee-detail

---
+++ projects/team/src/lib/adapters/primary/ui/employee-detail.component.ts
@@ -0,0 +1,5 @@
+import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({ selector: 'lib-employee-detail', templateUrl: './employee-detail.component.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
+export class EmployeeDetailComponent {
+}
